74ff0c5f5d4f301fe2ea0885e429c189eab162df
‘carlos’ <carlos.diegoticona@yahoo.com>
2023-04-23 16:41:12 -0300

    parte do carlos da barra de vaegação responsiva

--- src/index.js
+++ src/index.js
@@ -7,61 +7,67 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Alert from 'react-bootstrap/Alert';
+import AlertaVermelho from './navegacao';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+
+
 class App extends React.Component {
  
   render() {
     return (
       
-      <div className='banner'>
+      <Navbar  variant="white" expand="lg" className="navbar" style={{backgroundColor: "#546CCF", padding: "8px 10px"}}>
+      <Container>
+        <Navbar.Brand href="#home">
+        <img
+              src={Logosa}
+              width="100"
+              height="100"
+              className="d-inline-block align-text-top me-2"
+              alt="StudioAKI logo"
+            />
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" >
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Estúdios</Nav.Link>
+            <Nav.Link href="#link">Contato</Nav.Link>
+            <Nav.Link href="#link">Espaço Disponível</Nav.Link>
+            <Nav.Link href="#link">Espaço Disponível</Nav.Link>
+            </Nav>
+
+            <NavDropdown className="ms-auto" title="Conta" id="basic-nav-dropdown" >
+              <NavDropdown.Item href="#action/3.1">Minha conta</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#action/3.3" >
+              <AlertaVermelho>Sair</AlertaVermelho>
+              </NavDropdown.Item>
+            </NavDropdown>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+
+        
+
+        
       
-      <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
 
-        <div class="container-fluid" >
-          <a class="navbar-brand" href="#">
-            <img src={Logosa} alt="" width="100" height="100" class="d-inline-block align-text-top me-2"/>
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active text-dark" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-dark"  href="#">Ensaio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-dark"  href="#">Gravação</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-dark"  href="#">Contato</a>
-              </li>
-                
-            </ul>
-          </div>
-          
-        </div>
-      </nav>
+      
 
-        
 
-        
-      {/* Texto só pra ter uma noção */}
-
-        <div className="content">
-          <h1>ENCONTRE SEU STUDIO</h1>
-          <p>seja para gravar ou ensaiar,
-            <br/> aqui você vai encontrar o studio que esta peocurando</p>
-            <div>
-              <button type='button'><span> </span> SAIBA MAIS</button>
-              <button type='button'><span> </span>VEJA OS STUDIOS</button>
-            </div>
-          
-        </div>
-
-      </div>
-
+      
+      
     )
   }
 }
